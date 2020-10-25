c1752a46d062538c63871e9e4541431c114270e5
Daniel Archer <contato@danielcramer.com.br>
2020-10-25 13:00:45 -0400

    tls config for https

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"log"
 	"net/http"
 	"time"
@@ -9,11 +10,32 @@ import (
 )
 
 func main() {
+	tlsConfig := &tls.Config{
+		// Causes servers to use Go's default cipher suite preferences,
+		// which are tuned to avoid attacks. Does nothing on clients.
+		PreferServerCipherSuites: true,
+		// Only use curves which have assembly implementations
+		CurvePreferences: []tls.CurveID{
+			tls.CurveP256,
+			tls.X25519, // Go 1.8 only
+		},
+
+		MinVersion: tls.VersionTLS12,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		},
+	}
 	srv := &http.Server{
 		Addr: "",
 		ReadTimeout: 5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 		IdleTimeout: 120 * time.Second,
+		TLSConfig: tlsConfig,
 		Handler: routes(),
 	}
 	err := srv.ListenAndServe()
